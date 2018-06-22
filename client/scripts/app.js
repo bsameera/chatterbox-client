cfef5f2e893a567e5a31b44acb066e5932f43351
bsameera <bsameera79@gmail.com>
2018-06-22 16:32:55 -0700

    Use POST and GET methods

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,24 @@
 // YOUR CODE HERE:
+var app = { 
+  init: function() {},
+  send: function(message) {
+    $.ajax({
+      type: 'POST',
+      url: 'http://parse.hrsf.hackreactor.com/chatterbox/classes/messages',
+      data: JSON.stringify(message),
+      contentType: 'application/json'
+    });
+  },
+  fetch: function() {
+    $.ajax({
+      type: 'GET',
+      server: 'http://parse.hrsf.hackreactor.com/chatterbox/classes/messages',
+      contentType: 'application/json'
+    });
+  }
+
+};
+
+
+
+
