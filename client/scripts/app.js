6550826ecf80707bdb0fe183de17e04e818e5681
bsameera <bsameera79@gmail.com>
2018-06-22 20:07:57 -0700

    Implement clearMessages, renderMessages, renderRoom

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,3 +1,4 @@
+
 class App {
 
   init() {}
@@ -5,19 +6,55 @@ class App {
   send(message) {
     $.ajax({
       type: 'POST',
-      url: 'http://parse.hrsf.hackreactor.com/chatterbox/classes/messages',
+      url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
       data: JSON.stringify(message),
-      contentType: 'application/json'
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message', data);
+      }
     });
   }
+
   
   fetch() {
     $.ajax({
       type: 'GET',
-      server: 'http://parse.hrsf.hackreactor.com/chatterbox/classes/messages',
-      contentType: 'application/json'
+      server: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message fetched');
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to get message', data);
+      }
     });
   }
+
+  clearMessages() {
+    $('#chats').children().remove();
+  }
+  
+  renderMessage(message) {
+    let userName = message.username;
+    let userText = message.text;
+    let roomName = message.roomname;
+    let appendingText = `<div><h1>${userName}</h1><p>${userText}</p><p>${roomName}</p></div>`; 
+    $( '#chats' ).append(appendingText);
+  }
+
+  renderRoom(roomName) {
+    $('#roomSelect').append(`<div>${roomName}</div>`);
+  }
+
+  handleUsernameClick() {
+
+  }
+  
 }
 const app = new App();
 
